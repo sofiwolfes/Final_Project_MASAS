69221eae176a15b27f9b2a1723912ba96552945b
ShahZ-Khan <114197558+ShahZ-Khan@users.noreply.github.com>
2023-03-09 22:21:13 -0500

    Tables In SQL

---
+++ Final_Project.sql
@@ -0,0 +1,8 @@
+Create Table SPED_Retention_1819(
+"Student Information" Decimal (5,3),
+"District Non SPED Student Count" Decimal (5,3),
+"District Non SPED Percentage of Total Students" Decimal (5,3),
+"State SPED Student Count" Decimal (5,3),
+"State SPED Percentage of Total Students" Decimal (5,3)
+);
+Select * From sped_retention_1819
