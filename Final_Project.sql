5164ee3484570d3867d75f90a854a2092210d317
ShahZ-Khan <114197558+ShahZ-Khan@users.noreply.github.com>
2023-03-14 19:38:05 -0400

    Tables createdYOY_1921 YOY_1819 YOY_2122

--- Final_Project.sql
+++ Final_Project.sql
@@ -1,8 +1,81 @@
-Create Table SPED_Retention_1819(
-"Student Information" Varchar (100),
-"District Non SPED Student Count" Decimal (5,3),
-"District Non SPED Percentage of Total Students" Decimal (5,3),
-"State SPED Student Count" Decimal (5,3),
-"State SPED Percentage of Total Students" Decimal (5,3)
+Create Table YOY_2122_combined(
+"Primary_Key" Varchar (2000),
+"School_Name" Varchar (2000),
+"Grade_STAAR_Test" Varchar (2000),
+"Mastery_Level" Varchar (2000),
+"School_Year" NUMERIC,
+"YOY_State" NUMERIC,
+"State" NUMERIC,
+"YOY_Region" NUMERIC,
+"Region_04" NUMERIC,
+"YOY_District" NUMERIC,
+"District" NUMERIC,
+"YOY_African_American" NUMERIC,
+"African_American" NUMERIC,
+"YoY_Hispanic" NUMERIC,
+"Hispanic" NUMERIC,
+"YoY_White" NUMERIC,
+"White" NUMERIC,
+"YoY_Econ_Disadv" NUMERIC,
+"Econ_Disadv" NUMERIC,
+"YoY_ELL" NUMERIC,
+"ELL" NUMERIC
+
 );
-Select * From sped_retention_1819
+
+Create Table YOY_1819_combined(
+"Primary_Key" Varchar (2000),
+"School_Name" Varchar (2000),
+"Grade_STAAR_Test" Varchar (2000),
+"Mastery_Level" Varchar (2000),
+"School_Year" NUMERIC,
+"YOY_State" NUMERIC,
+"State" NUMERIC,
+"YOY_Region" NUMERIC,
+"Region_04" NUMERIC,
+"YOY_District" NUMERIC,
+"District" NUMERIC,
+"YOY_African_American" NUMERIC,
+"African_American" NUMERIC,
+"YoY_Hispanic" NUMERIC,
+"Hispanic" NUMERIC,
+"YoY_White" NUMERIC,
+"White" NUMERIC,
+"YoY_Econ_Disadv" NUMERIC,
+"Econ_Disadv" NUMERIC,
+"YoY_ELL" NUMERIC,
+"ELL" NUMERIC
+);
+
+Create Table YOY_1921_combined(
+"Primary_Key" Varchar (2000),
+"School_Name" Varchar (2000),
+"Grade_STAAR_Test" Varchar (2000),
+"Mastery_Level" Varchar (2000),
+"School_Year" NUMERIC,
+"YOY_State" NUMERIC,
+"State" NUMERIC,
+"YOY_Region" NUMERIC,
+"Region_04" NUMERIC,
+"YOY_District" NUMERIC,
+"District" NUMERIC,
+"YOY_African_American" NUMERIC,
+"African_American" NUMERIC,
+"YoY_Hispanic" NUMERIC,
+"Hispanic" NUMERIC,
+"YoY_White" NUMERIC,
+"White" NUMERIC,
+"YoY_Econ_Disadv" NUMERIC,
+"Econ_Disadv" NUMERIC,
+"YoY_ELL" NUMERIC,
+"ELL"  NUMERIC
+);
+
+Select * From YOY_1921_combined
+Select * From YOY_1819_combined
+Select * From YOY_2122_combined
+
+
+
+
+
